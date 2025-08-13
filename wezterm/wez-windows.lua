795301564b51371d134ce3955ee23999807eb636
Patrick Dawson <patrick.dawson79@gmail.com>
2025-08-13 08:21:29 +0200

    Updates default fonts
    Updates the default font configuration to use "JetBrains Mono" and "MesloLGS Nerd Font" at a scale of 1.0.
    This provides a more consistent and visually appealing default font experience.

--- wezterm/wez-windows.lua
+++ wezterm/wez-windows.lua
@@ -5,8 +5,8 @@ local module = {}
 function module.apply_to_config(config)
 	-- override font for scaling
 	config.font = wezterm.font_with_fallback({
-		{ family = "CaskaydiaCove Nerd Font", scale = 1.1 },
-		{ family = "JetBrains Mono", scale = 1.2 },
+		{ family = "JetBrains Mono", scale = 1.0 },
+		{ family = "MesloLGS Nerd Font", scale = 1.0 },
 	})
 
 	-- launcher menu
